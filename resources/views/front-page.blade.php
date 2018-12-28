48cc31549a71daf0737750dd85b4fedec132ccb9
recreal <recreal.webmedia@gmail.com>
2018-12-28 16:04:12 +0900

    投稿ループのレイアウトを3カラム2カラムに変更

--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -10,9 +10,11 @@
     {!! get_search_form(false) !!}
   @endif
 
-  @while (have_posts()) @php the_post() @endphp
+  <div class="row">
+  @while (have_posts()) @php the_post() @endphp  
     @include('partials.content-'.get_post_type())
   @endwhile
+  </div>
 
   {!! get_the_posts_navigation() !!}
 @endsection
