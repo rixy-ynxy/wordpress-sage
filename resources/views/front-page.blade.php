1d54e2ad958f9b6bb4314cd6b1e11ba6dea332d6
recreal <recreal.webmedia@gmail.com>
2018-12-31 00:05:15 +0900

    フロントページの複数ループ対応

--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -9,30 +9,19 @@
     </div>
     {!! get_search_form(false) !!}
   @endif
-
-  <div class="row">
+    <div class="row">
   @while (have_posts()) @php the_post() @endphp  
     @include('partials.content-'.get_post_type())
   @endwhile
   </div>
-
-  {!! get_the_posts_navigation() !!}
-@endsection
-@section('content')
   @include('partials.page-header')
-
-  @if (!have_posts())
-    <div class="alert alert-warning">
-      {{ __('Sorry, no results were found.', 'sage') }}
-    </div>
-    {!! get_search_form(false) !!}
-  @endif
-
   <div class="row">
-  @while (have_posts()) @php the_post() @endphp  
-    @include('partials.content-'.get_post_type())
+  @while($members_loop->have_posts()) @php($members_loop->the_post())
+    @include('partials.content-list')
   @endwhile
+  @php(wp_reset_postdata())
   </div>
+
 
   {!! get_the_posts_navigation() !!}
 @endsection
