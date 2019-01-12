b7f121b324bf73e9256527f97f540dafa0565fb1
recreal <recreal.webmedia@gmail.com>
2019-01-12 17:35:39 +0900

    メンバー一覧のときだけauthor出力をオフ

--- resources/views/partials/entry-meta.blade.php
+++ resources/views/partials/entry-meta.blade.php
@@ -1,6 +1,9 @@
 <time class="updated" datetime="{{ get_post_time('c', true) }}">{{ get_the_date() }}</time>
-<p class="byline author vcard">
+@if ( !is_post_type_archive('members'))
+<p class="ibyline author vcard">
   <a href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" rel="author" class="fn">
     {{ get_the_author() }}
   </a>
 </p>
+@else
+@endif
