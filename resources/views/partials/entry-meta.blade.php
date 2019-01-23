47fc99f45cc06093f9062cac74a62d5d2a70f8c8
recreal <recreal.webmedia@gmail.com>
2019-01-23 18:01:43 +0900

    phpコード追加

--- resources/views/partials/entry-meta.blade.php
+++ resources/views/partials/entry-meta.blade.php
@@ -1,4 +1,7 @@
 <time class="updated" datetime="{{ get_post_time('c', true) }}">{{ get_the_date() }}</time>
+<div class="posts-cat">
+  @php the_category(' / ') @endphp
+</div>
 @if ( !is_post_type_archive('members'))
 <p class="ibyline author vcard">
   <a href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" rel="author" class="fn">
