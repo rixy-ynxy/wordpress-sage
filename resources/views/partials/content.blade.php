7e47cc84474868cceb5a7816a734437aa6e7d686
recreal <recreal.webmedia@gmail.com>
2019-01-26 04:28:03 +0900

    投稿一覧にユーザーアイコン、カテゴリー表示とレイアウト調整

--- resources/views/partials/content.blade.php
+++ resources/views/partials/content.blade.php
@@ -1,9 +1,17 @@
 <article @php post_class('archive') @endphp>
+  @if ( !is_post_type_archive('members'))
+  <p class="ibyline author vcard">
+    <a href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" rel="author" class="fn">
+      @php echo get_avatar(get_the_author_meta('ID'), 50) @endphp
+    </a>
+  </p>
+  @else
+  @endif
   <header>
     <h2 class="entry-title"><a href="{{ get_permalink() }}">{{ get_the_title() }}</a></h2>
-    
+    <div class="entry-summary">
+      @include('partials/entry-meta')
+    </div>
   </header>
-  <div class="entry-summary">
-    @include('partials/entry-meta')
-  </div>
+
 </article>
--- resources/views/partials/entry-meta.blade.php
+++ resources/views/partials/entry-meta.blade.php
@@ -2,11 +2,4 @@
 <div class="posts-cat">
   @php the_category(' / ') @endphp
 </div>
-@if ( !is_post_type_archive('members'))
-<p class="ibyline author vcard">
-  <a href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" rel="author" class="fn">
-    {{ get_the_author() }}
-  </a>
-</p>
-@else
-@endif
+
