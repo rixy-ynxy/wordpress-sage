1c8d73471a4ed75bc87c19b4252a6d267c74d186
Yuki Kakefuda <yuppy@YukinoiMac.local>
2019-01-26 21:56:05 +0900

    pagination確認お願いします

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,5 +14,13 @@
     @include('partials.content-'.get_post_type())
   @endwhile
   </div>
-  {!! get_the_posts_navigation() !!}
+  <!-- ページネーション -->
+  <div class="pagination">
+    @php echo paginate_links( array(
+      'type' => 'list',
+      'mid_size' => '1',
+      'prev_text' => '&laquo;',
+      'next_text' => '&raquo;'
+    )) @endphp
+  </div>
 @endsection
