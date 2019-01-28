af3f54c3b823c9d02e07c1ab7d7bac0f95c39704
moochan <hippopo.web@gmail.com>
2019-01-28 15:36:42 +0900

    ロゴサイズをfeatureからマージ、タブレットサイズ以下での表示の調整

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,7 +2,7 @@
   <div class="container">
     @php the_custom_logo() @endphp
     <nav class="navbar navbar-expand-md navbar-light bg-faded">
-      <span class="navbar-brand mb-0 h1">RemoCA</span>
+      <!-- <span class="navbar-brand mb-0 h1">RemoCA</span> -->
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs4navbar" aria-controls="bs4navbar" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"><i class="fas fa-bars"></i></span>
       </button>
