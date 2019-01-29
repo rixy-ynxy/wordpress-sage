59f5764cd5aa2ce5748809166dbd613e9a0474cb
recreal <recreal.webmedia@gmail.com>
2019-01-30 01:54:07 +0900

    footerのウィジェット化

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,38 +1,15 @@
 <footer class="content-info">
   <div class="container">
     <section id="footerAds">
-      <div class="row">
-      @include('partials.sidebar-footer')
+      <div class="row text-center">
+        @include('partials.sidebar-footerads')
       </div>
-
-			<div class="card-deck">
-				<div class="card" style="width: 18rem;">
-  				<a href="#" class="text-decoration-none">
-						<img class="card-img-top" src="http://remocamedia.local/wp-content/uploads/2019/01/RemoCA-300x300.png" alt="カードの画像">
-  						<div class="card-body">
-    						<p class="card-text">カードのタイトルに基づいて、カードのコンテンツの大部分を占める簡単なサンプルテキスト。</p>
-  						</div>
-					</a>
-				</div>
-				<div class="card" style="width: 18rem;">
-					<a href="#" class="text-decoration-none">
-  					<img class="card-img-top" src="http://remocamedia.local/wp-content/uploads/2019/01/RemoCA-300x300.png" alt="カードの画像">
-  						<div class="card-body">
-    						<p class="card-text">カードのタイトルに基づいて、カードのコンテンツの大部分を占める簡単なサンプルテキスト。</p>
-  						</div>
-					</a>
-				</div>
-				<div class="card" style="width: 18rem;">
-					<a href="#" class="text-decoration-none">
-  					<img class="card-img-top" src="http://remocamedia.local/wp-content/uploads/2019/01/RemoCA-300x300.png" alt="カードの画像">
-  						<div class="card-body">
-    						<p class="card-text">カードのタイトルに基づいて、カードのコンテンツの大部分を占める簡単なサンプルテキスト。</p>
-  						</div>
-					</a>
-				</div>
-			</div>
     </section>
-
+    <section id="footer-menu">
+      <div class="row">
+        @include('partials.sidebar-footer')
+      </div>
+    </section>
     <nav class="navbar navbar-expand-md navbar-light bg-faded">
       @if (has_nav_menu('footer_navigation'))
         {!! wp_nav_menu([
@@ -47,14 +24,13 @@
 				]) !!}
       @endif
     </nav>
-		<div>
-			<p class="text-center small">
+		<p class="text-center small">
 				&copy;2018
         @if (date("Y")!=2018)
           @php echo date("-Y"); @endphp
         @endif
          |
-        @php bloginfo('name') @endphp</p>
-		</div>
+        @php bloginfo('name') @endphp
+    </p>
   </div>
 </footer>
