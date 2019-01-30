5d87541d9b4405efcf2df39415204da4c49a7249
Yuki Kakefuda <yuppy@YukinoiMac.local>
2019-01-30 17:23:30 +0900

    head.blade.phpに、アイキャッチ画像OGPタグを挿入

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,5 +3,17 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+  
+  <!-- OGP 個別ページ　アイキャッチ画像 -->
+  <?php if( is_single() || is_page() ): ?>
+  <?php setup_postdata($post) ?>
+    <meta property="og:type" content="article">
+    <meta property="og:title" content="<?php the_title(); ?>">
+    <meta property="og:url" content="<?php the_permalink(); ?>">
+    <?php if( has_post_thumbnail() ): ?>
+      <?php $postthumb = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large' ); ?>
+      <meta property="og:image" content="<?php echo $postthumb[0]; ?>">
+    <?php endif; ?>
+  <?php endif; ?>
   @php wp_head() @endphp
 </head>
