8524025b8e9957a9e1f917d68eed9fa1bf2b0886
bamb00 <kyoto0707@gmail.com>
2019-01-30 16:41:14 +0900

    フッターナビのレスポンシブ対応

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -10,20 +10,21 @@
         @include('partials.sidebar-footer')
       </div>
     </section>
-    <nav class="navbar navbar-expand-md navbar-light bg-faded">
+    <div id="footer-nav">
       @if (has_nav_menu('footer_navigation'))
         {!! wp_nav_menu([
 					'menu'            => 'ナビゲーションバー',
 					'theme_location'  => 'footer_navigation',
 					'container'       => 'div',
+					'container_id'    => 'bs4navbar',
 					'menu_id'         => false,
-					'menu_class'      => 'navbar-nav',
+					'menu_class'      => 'nav justify-content-center',
 					'depth'           => 2,
 					'fallback_cb'     => 'bs4navwalker::fallback',
 					'walker'          => new bs4navwalker()
 				]) !!}
       @endif
-    </nav>
+		</div>
 		<p class="text-center small">
 				&copy;2018
         @if (date("Y")!=2018)
