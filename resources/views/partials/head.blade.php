0fdc3542542726aff6f57328e397f4b033b45790
recreal <recreal.webmedia@gmail.com>
2019-02-08 21:25:11 +0900

    adsenseをheadに追加

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -23,4 +23,11 @@
     <?php endif; ?>
   <?php endif; ?>
   @php wp_head() @endphp
+  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  <script>
+      (adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: "ca-pub-8583945528952172",
+            enable_page_level_ads: true
+      });
+  </script>
 </head>
