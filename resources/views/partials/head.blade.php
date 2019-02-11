50a272dd7e6d97556105cee6beec05f7202f59d7
recreal <recreal.webmedia@gmail.com>
2019-02-11 11:42:01 +0900

    アカウント変更

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -4,8 +4,8 @@
   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-5GPV993');</script>
-  <!-- End Google Tag Manager -->  
+  })(window,document,'script','dataLayer','GTM-TXC5DPW');</script>
+  <!-- End Google Tag Manager -->
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
