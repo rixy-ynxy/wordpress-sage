348e9f2a2a06bac7045e6f006f472bc264cc18f9
recreal <recreal.webmedia@gmail.com>
2019-02-17 14:40:23 +0900

    entry-metaレイアウト修正、アイコン表記を追加

--- resources/views/partials/entry-meta.blade.php
+++ resources/views/partials/entry-meta.blade.php
@@ -1,3 +1,9 @@
+<div class="author-box">
+<p class="ibyline author vcard">
+  <a href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" rel="author" class="fn">
+    @php echo get_avatar(get_the_author_meta('ID'), 50) @endphp
+  </a>
+</p>
 <div class="metaBox">
   @if ( !is_post_type_archive('members'))
     <time class="updated" datetime="{{ get_post_time('c', true) }}">{{ get_the_date() }}</time>
@@ -7,4 +13,5 @@
   @if (  !is_front_page() || !is_home() )
     @php the_tags( '<ul class="entry-tag"><li>', '</li><li>', '</li></ul>') @endphp
   @endif
+</div>
 </div>
