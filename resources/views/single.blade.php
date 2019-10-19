876774de881dcefb675af38f608d983c3a58d5b4
Yunixy <yunixy.dev@gmail.com>
2019-10-20 03:39:17 +0900

    個別ページにもsidebar追加

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -1,7 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-  @while(have_posts()) @php the_post() @endphp
-    @include('partials.content-single-'.get_post_type())
-  @endwhile
+<div class="row">
+  <div class="col-md-8">
+    @while(have_posts()) @php the_post() @endphp
+      @include('partials.page-header')
+      @include('partials.content-single')
+    @endwhile
+  </div>
+  <div class="col-md-4">
+    @php dynamic_sidebar('sidebar-primary') @endphp
+  </div>
+</div>
 @endsection
