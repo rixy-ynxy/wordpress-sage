f91e2341e9917429ba75c324d11d44d7f3ccd363
Yunixy <yunixy.dev@gmail.com>
2019-10-19 09:41:36 +0900

    グリッド変更

--- resources/woocommerce/checkout/form-checkout.php
+++ resources/woocommerce/checkout/form-checkout.php
@@ -36,7 +36,7 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 		<?php do_action( 'woocommerce_checkout_before_customer_details' ); ?>
 
 		<div class="col2-set row" id="customer_details">
-			<div class="col-6">
+			<div class="col-md-6">
 				<?php do_action( 'woocommerce_checkout_billing' ); ?>
         <?php do_action( 'woocommerce_checkout_shipping' ); ?>
 			</div>
