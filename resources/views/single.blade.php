47394a2016fe2d06c72ac55ba1adfd149ac2d0d0
Yunixy <yunixy.dev@gmail.com>
2019-10-20 03:42:12 +0900

    固定ページのタイトル重複を解消

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -4,7 +4,6 @@
 <div class="row">
   <div class="col-md-8">
     @while(have_posts()) @php the_post() @endphp
-      @include('partials.page-header')
       @include('partials.content-single')
     @endwhile
   </div>
