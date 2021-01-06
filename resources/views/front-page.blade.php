2f7727b623eb766bb7d54cf5a5a2ed455eafb385
Tokiwa_Ryuji <ryuji_tokiwa@yunixy.co.jp>
2021-01-06 21:11:10 +0900

    front-page layout edit

--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -1,18 +1,20 @@
 @extends('layouts.front-app')
 
 @section('content')
-  @include('partials.page-header')
-  @if (!have_posts())
-    <div class="alert alert-warning">
-      {{ __('Sorry, no results were found.', 'sage') }}
-    </div>
-    {!! get_search_form(false) !!}
-  @endif
+  
+  <div class="col-md-8">
+    @if (!have_posts())
+      <div class="alert alert-warning">
+        {{ __('Sorry, no results were found.', 'sage') }}
+      </div>
+      {!! get_search_form(false) !!}
+    @endif
 
-  @while (have_posts()) @php the_post() @endphp  
-    @include('partials.content-'.get_post_type())
-  @endwhile
-
+    @while (have_posts()) @php the_post() @endphp  
+      @include('partials.content-'.get_post_type())
+    @endwhile
+  </div>
+  
   <div class="col-md-4">
     @php dynamic_sidebar('sidebar-primary') @endphp
   </div>
