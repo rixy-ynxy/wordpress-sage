fc8f27eba356018902d3b12e686a726ea94b276c
Tokiwa_Ryuji <ryuji_tokiwa@yunixy.co.jp>
2021-01-06 21:12:45 +0900

    front-page css edit

--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -1,22 +1,24 @@
 @extends('layouts.front-app')
 
 @section('content')
-  
-  <div class="col-md-8">
-    @if (!have_posts())
-      <div class="alert alert-warning">
-        {{ __('Sorry, no results were found.', 'sage') }}
-      </div>
-      {!! get_search_form(false) !!}
-    @endif
+  <div class="row">
+    <div class="col-md-8">
+      @if (!have_posts())
+        <div class="alert alert-warning">
+          {{ __('Sorry, no results were found.', 'sage') }}
+        </div>
+        {!! get_search_form(false) !!}
+      @endif
 
-    @while (have_posts()) @php the_post() @endphp  
-      @include('partials.content-'.get_post_type())
-    @endwhile
+      @while (have_posts()) @php the_post() @endphp  
+        @include('partials.content-'.get_post_type())
+      @endwhile
+    </div>
+    
+    <div class="col-md-4">
+      @php dynamic_sidebar('sidebar-primary') @endphp
+    </div>
   </div>
   
-  <div class="col-md-4">
-    @php dynamic_sidebar('sidebar-primary') @endphp
-  </div>
 
 @endsection
